8316f70370b5f4b80305ac4eb599533134351687
Gingertonic <bethmschofield@gmail.com>
2020-02-08 15:38:24 +0000

    reset

--- src/components/ProjectIndex.js
+++ src/components/ProjectIndex.js
@@ -8,11 +8,11 @@ class ProjectIndex extends Component {
     }
 
     handleClick = e => {
-        this.props.loadProject(e.target.id)
+        // this.props.loadProject(e.target.id)
     }
 
     render() {
-        const renderProjects = this.props.projects.map(p => <span onClick={this.handleClick()}>{p.header}</span>)
+        const renderProjects = this.props.projects.map(p => { <span onClick={this.handleClick()}>{p.header}</span> }) 
         return (
             <div id="all-projects">
                 renderProjects
